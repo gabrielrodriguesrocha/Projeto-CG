695fa277adfa15a412a1965abc02f372f34bd84a
gabrielrodriguesrocha <gabrielrodrigues_rocha@hotmail.com>
2018-04-10 12:12:51 -0300

    Add skeleton files

---
+++ Dist/Sources/main.cpp
@@ -0,0 +1,94 @@
+// Local Headers
+#include "glitter.hpp"
+#include "shader.hpp"
+
+// System Headers
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// Standard Headers
+#include <cstdio>
+#include <cstdlib>
+
+// Object Vertices
+float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	0.5f, -0.5f, 0.0f,
+	0.0f,  0.5f, 0.0f
+};
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+int main(int argc, char * argv[]) {
+
+    // Load GLFW and Create a Window
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    auto mWindow = glfwCreateWindow(mWidth, mHeight, "OpenGL", nullptr, nullptr);
+
+    // Check for Valid Context
+    if (mWindow == nullptr) {
+        fprintf(stderr, "Failed to Create OpenGL Context");
+        return EXIT_FAILURE;
+    }
+
+    // Create Context and Load OpenGL Functions
+    glfwMakeContextCurrent(mWindow);
+    gladLoadGL();
+    fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
+	glfwSetFramebufferSizeCallback(mWindow, framebuffer_size_callback);
+
+	// Creating and Binding the Vertex Array Object (VAO)
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	// Creating and Binding the Vertex Buffer Object (VBO)
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	// Copy User Data Into Buffer
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Set Vertex Attribute Pointers
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// Build and Compile Shaders
+	// -------------
+	Mirage::Shader shader;
+	shader.attach("main.vert").attach("main.frag");
+	shader.link();
+	shader.activate();
+
+    // Rendering Loop
+    while (glfwWindowShouldClose(mWindow) == false) {
+        if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(mWindow, true);
+
+        // Background Fill Color
+        glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+		
+		shader.activate();
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // Flip Buffers and Draw
+        glfwSwapBuffers(mWindow);
+        glfwPollEvents();
+    }   glfwTerminate();
+    return EXIT_SUCCESS;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
---
+++ Dist/Vendor/README.md
@@ -0,0 +1,12 @@
+# Dependências do Projeto
+
+Este diretório contém os [submódulos necessários](/.gitmodules) para a compilação do projeto. Como as dependências do projeto foram herdadas do repositório [Glitter](https://github.com/Polytonic/Glitter/), as dependências deste projeto são as mesmas daquele:
+
+Funcionalidade                  | Biblioteca
+------------------------------- | ------------------------------------------
+Carregamento de Mesh            | [assimp](https://github.com/assimp/assimp)
+Física                          | [bullet](https://github.com/bulletphysics/bullet3)
+Carregamento de Funções OpenGL  | [glad](https://github.com/Dav1dde/glad)
+Janelas e Entrada               | [glfw](https://github.com/glfw/glfw)
+Operações Matemáticas           | [glm](https://github.com/g-truc/glm)
+Carregamento de Texturas        | [stb](https://github.com/nothings/stb)
