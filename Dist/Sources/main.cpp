8d3319fc2733bd3675b684fbaa1dc7fe8d5edaaa
hskodama <hskodama@hotmail.com>
2018-05-09 02:23:15 -0300

    Documentation
    Added half of the main documentation

--- Dist/Sources/main.cpp
+++ Dist/Sources/main.cpp
@@ -18,16 +18,16 @@ void processInput(GLFWwindow *window);
 
 int main(int argc, char * argv[]) {
 
-    // Load GLFW and Create a Window
+    // Initiate the GLFW library
     glfwInit();
 
-    /*
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-	*/
+	/* Create window with:
+	 * Width (int) = mWidth
+	 * Height (int) = mHeight
+	 * Title (const char*) = "OpenGL"
+	 * Monitor (GLFWmonitor *) = nullptr
+	 * Share (GLFWmonitor *) = nullptr
+	 */
     auto mWindow = glfwCreateWindow(mWidth, mHeight, "OpenGL", nullptr, nullptr);
 
     // Check for Valid Context
@@ -36,34 +36,57 @@ int main(int argc, char * argv[]) {
         return EXIT_FAILURE;
     }
 
-    // Create Context and Load OpenGL Functions
+    // Create Context for the current window
     glfwMakeContextCurrent(mWindow);
+    
+    // Load OpenGL
     gladLoadGL();
     fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
+    
+    // Set the framebuffer size callback of the current window
 	glfwSetFramebufferSizeCallback(mWindow, framebuffer_size_callback);
 
-	// Build and Compile Shaders
-	// -------------
+
+	/* Build and Compile Shaders
+	 * -------------------------
+	 */
 	Mirage::Shader shader;
 	shader.attach("main.vert").attach("main.frag");
 	shader.link();
 
+	// Load the mesh (collectoin of vertices) of each object
 	Mirage::Mesh stormtrooper("stormtrooper.obj");
 	printf("%0.2f %0.2f %0.2f\n", (stormtrooper.getCenter()).x, (stormtrooper.getCenter()).y, (stormtrooper.getCenter()).z);
 	Mirage::Mesh monkey("monkey.obj");
-	printf("%0.2f %0.2f %0.2f\n", (monkey.getCenter()).x, (monkey.getCenter()).y, (monkey.getCenter()).z);
+	printf("%0.2f %0.2f %0.2f\n", (stormtrooper.getCenter()).x, (stormtrooper.getCenter()).y, (stormtrooper.getCenter()).z);
 
-
+	
+	/* 
+	 * glEnable(GL_DEPTH_TEST); -> Do depth comparisons and update the depth buffer
+	 * glEnable(GL_CULL_FACE); 	-> Cull polygons based on their winding in window coordinates
+	 * glCullFace(GL_BACK);		-> Back-facing polygons are candidates for culling
+	 */
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
 	glCullFace(GL_BACK);
 
+	
 	int param = 0;
-    // Rendering Loop
+	
+    /* Rendering Loop
+	 * --------------
+	 *
+	 * 	glfwWindowShouldClose(mWindow) return the value of "close" flag from the window
+     */
     while (glfwWindowShouldClose(mWindow) == false) {
+    	// Verify the permanence of the loop
 		processInput(mWindow);
 
-        // Background Fill Color
+        /* 
+         * glClearColor(1.0f, 1.0f, 1.0f, 1.0f);-> Specify clear values for the color buffers
+         * glClear(GL_COLOR_BUFFER_BIT);		-> Clear the buffers enabled for color writing to preset value
+         * glClear(GL_DEPTH_BUFFER_BIT);		-> Clear the depth buffer to preset value
+         */
         glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 		glClear(GL_DEPTH_BUFFER_BIT);
@@ -71,26 +94,58 @@ int main(int argc, char * argv[]) {
 		//newvalue = sin(param*M_PI/180)*10)
 		shader.activate();
 
-		// Model Matrices
-		glm::mat4 modelStormtrooper, modelMonkey;
-		modelStormtrooper = glm::scale(glm::translate(modelStormtrooper, glm::vec3(-stormtrooper.getCenter().x +(0.0f),-stormtrooper.getCenter().y +(0.0f), -stormtrooper.getCenter().z +(-5.0f))), glm::vec3(glm::abs(sin(param*M_PI/180))+1, glm::abs(sin(param*M_PI/180))+1, glm::abs(sin(param*M_PI/180))+1));
-		modelMonkey = glm::rotate(glm::translate(modelMonkey, glm::vec3(-monkey.getCenter().x +(sin(param*M_PI/180)*4),-monkey.getCenter().y + 0.0f, -monkey.getCenter().z +((cos(param*M_PI/180)*4)-5.0f))), 7.0f, glm::vec3(0.0f, 1.0f, 0.0f));
-		
-		// View Matrices
-		glm::mat4 view;
-		view = glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), // eye
-						   glm::vec3(0.0f, 0.0f, -3.0f), // center
-						   glm::vec3(0.0f, 1.0f, 0.0f));// up
 
-		// Projection Matrices
+		/* Model Matrices
+		 * --------------
+		 */ 
+		glm::mat4 modelStormtrooper, modelMonkey;
+		
+		/*
+		 * Create a 4*4 transformation matrix from a vector of 3 components containing 
+		 * the center of the object on each axis through max/min interpolation
+		 */
+		modelStormtrooper = glm::translate(modelStormtrooper, glm::vec3(-stormtrooper.getCenter().x +(0.0f),-stormtrooper.getCenter().y +(0.0f), -stormtrooper.getCenter().z +(-5.0f)));
+		modelMonkey = glm::translate(modelMonkey, glm::vec3(-monkey.getCenter().x +(sin(param*M_PI/180)*4),-monkey.getCenter().y +(0.0f), -monkey.getCenter().z +((cos(param*M_PI/180)*4)-5.0f)));;
+		
+		
+		/*
+		 * View Matrices
+	 	 * -------------
+	 	 *
+	 	 * view define the axes of the coordinate system through 3 vectors:
+	 	 *	- Eye: position of the camera's view point
+	 	 *	- Center: point looked
+	 	 *	- Up: defines the Y axis
+	 	 */ 
+		glm::mat4 view;
+		view = glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f),		// eye
+						   glm::vec3(0.0f, 0.0f, -3.0f), 	// center
+						   glm::vec3(0.0f, 1.0f, 0.0f));	// up
+
+
+		/*
+		 * Projection Matrices
+		 * -------------------
+		 *
+		 * perspective creates a projection matrix used to project the rendered scene onto the screen.
+		 * In this case a 75 degrees FOV, width/height aspect ratio of the screen, and a min/max 
+		 * (near/far) render points.
+		 */ 
 		glm::mat4 projection;
 		projection = glm::perspective(glm::radians(75.0f), (float) mWidth / mHeight, 0.1f, 100.0f);
 
-		// Normal Matrices
-		glm::mat3 normalStormtrooper, normalMonkey;
-		normalStormtrooper = glm::inverse(glm::mat3(view * modelStormtrooper)); 
-		normalMonkey = glm::inverse(glm::mat3(view * modelMonkey));
 
+		/*
+		 * Normal Matrices
+		 * ---------------
+		 *
+		 * normal contain the inverse of the 3*3 matrix from view*model
+		 */ 
+		glm::mat3 normal, normal2;
+		normal = glm::inverse(glm::mat3(view * modelStormtrooper)); 
+		normal2 = glm::inverse(glm::mat3(view * modelMonkey));
+
+		
 		int modelLoc = glGetUniformLocation(shader.get(), "modelMatrix");
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(modelStormtrooper));
 
@@ -101,7 +156,7 @@ int main(int argc, char * argv[]) {
 		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
 		int normalLoc = glGetUniformLocation(shader.get(), "normalMatrix");
-		glUniformMatrix3fv(normalLoc, 1, GL_TRUE, glm::value_ptr(normalStormtrooper));
+		glUniformMatrix3fv(normalLoc, 1, GL_TRUE, glm::value_ptr(normal));
 
 		int ambientLightColorU = glGetUniformLocation(shader.get(), "ambientLightColor");
 		int directionalLightU = glGetUniformLocation(shader.get(), "directionalLight");
@@ -134,7 +189,7 @@ int main(int argc, char * argv[]) {
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(modelMonkey));
 
 		normalLoc = glGetUniformLocation(shader.get(), "normalMatrix");
-		glUniformMatrix3fv(normalLoc, 1, GL_TRUE, glm::value_ptr(normalMonkey));
+		glUniformMatrix3fv(normalLoc, 1, GL_TRUE, glm::value_ptr(normal2));
 
 		monkey.draw(shader.get());
 
@@ -146,13 +201,15 @@ int main(int argc, char * argv[]) {
     return EXIT_SUCCESS;
 }
 
+
+/* processInput(GLFWwindow *window)
+ * If the last state reported for the ESC key on this window is 
+ * "GLFW_PRESS", set the "close" flag to true (terminate the loop)
+ */
 void processInput(GLFWwindow *window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
-
-	double xpos, ypos;
-	glfwGetCursorPos(window, &xpos, &ypos);
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
